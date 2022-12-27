2018eaf699d81eb5a512fd23026c3b4f75406cc9
Ranjit singh <jiradhey402@gmail.com>
2022-12-27 15:02:04 +0530

    api on progress

--- app.py
+++ app.py
@@ -5,12 +5,19 @@ import joblib
 import warnings
 warnings.filterwarnings('ignore')
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/recommend1')
+def recommend1():
+    return render_template('popularity.html')
 
 
-
+@app.route('/bookName')
+def show_book_name():
+    book_name = list(pd.read_csv('collaborative_models/unique_book_name.csv')['Book-Title'].values)
+    return render_template("bookname.html",list_fo_book_name = book_name)
 
 
 

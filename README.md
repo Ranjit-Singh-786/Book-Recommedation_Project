9bc04ba49d7e905ca72b6c28316c2831e70bc7e7
Ranjit singh <jiradhey402@gmail.com>
2022-12-27 22:31:19 +0530

    project completed with 2 recommandation

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Book-Recommedation_Project
-in this project, i build three kind of Recommandation System are popularity based , content based, collaborative filtering based Recommandation Systems.
+in this project, i build three kind of Recommandation System are :
+<ul>
+<li>popularity based filtering</li>
+<li>content based filtering </li>
+<li>collaborative based filtering </li>
+</ul>
+## Recommandation Systems
+It predicts the user’s interests and recommends products to the users that will<br> engage their attention. These are quite effective machine learning mechanisms that have helped businesses gain more revenue.
+### To install all Dependencies for this project execute this command. 
+""" pip install -r requirements.txt """
+
+### steps to execute this project
+<ul>
+    <li>clone this project.</li>
+    <li>go inside the current working directory.</li>
+    <li>open the command prompt at this location.</li>
+    <li>execute the "python app.py"</li>
+    <li>get the linc and past on your browser.</li>
+</ul>
+
+### click on the RECOMMANDATION-1 button to SEE popularity based filtering recommanded books.
+
+<img src ="static/a1.png" width = "600" height = "400">
+
+### click on the RECOMMANDATION-2 button to SEE collaborative based filtering recommanded books.
+
+<img src ="static/a2.png" width = "600" height = "400">
+
+### click on the BOOK-NAME button to SEE 574 famous books Name.
+
+<img src ="static/a3.png" width = "600" height = "400">
+
+### write books Name in input section and click on the submit button to get recommandation.
+
+<img src ="static/a4.png" width = "600" height = "400">
+##Thank you 🌝
--- app.py
+++ app.py
@@ -4,20 +4,68 @@ import pandas as pd
 import joblib
 import warnings
 warnings.filterwarnings('ignore')
-app = Flask(__name__)
+# load the models
+popular = pd.read_csv('popularity_based/popularity_based.csv')
+pt = joblib.load('collaborative_models/pivot_table_data.lb')
+similarity_scores = joblib.load('collaborative_models/similarity_score.lb')
+books = joblib.load('collaborative_models/collaborative_recmdsystem_data.lb')
 
+app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/recommend1')
-def recommend1():
-    return render_template('popularity.html')
-
-
+# to get all famous books name
 @app.route('/bookName')
 def show_book_name():
     book_name = list(pd.read_csv('collaborative_models/unique_book_name.csv')['Book-Title'].values)
     return render_template("bookname.html",list_fo_book_name = book_name)
+# to get the popularity based recommandation system
+@app.route('/recommend1')
+def recommend1():
+    return render_template('popularity.html',
+                           book_name = list(popular['Book-Title'].values),
+                           author=list(popular['Book-Author'].values),
+                           image=list(popular['Image-URL-M'].values),
+                           votes=list(popular['num_ratings'].values),
+                           rating=list(popular['avg_rating'].values),
+                           publisher=list(popular['Publisher'].values)
+                           
+                           )
+# to display the recommandation-2 html file
+@app.route('/recommend2')
+def recommend2():
+    return render_template('recomandation.html')
+
+# to get the recommandation by the recommandation-2
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    try:
+        user_input = request.form.get('user_input')
+        index = np.where(pt.index == user_input)[0][0]
+        similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:9]
+    except:
+        return render_template("errorrr.html")
+    else:
+        data = []
+        for i in similar_items:
+            item = []
+            temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Publisher'].values))
+            item.extend(list(temp_df.drop_duplicates('Book-Title')['Year-Of-Publication'].values))
+
+
+            data.append(item)
+        # print(data)
+        return render_template('recomandation.html',data=data)
+
+
+
+
+
+
 
 
 
